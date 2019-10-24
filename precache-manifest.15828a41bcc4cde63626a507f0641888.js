c1b25922d0642897b33401d05f327bc98af0a55c
Carlos <carloshximenes@gmail.com>
2019-10-23 21:31:58 -0300

    Updates

---
+++ precache-manifest.15828a41bcc4cde63626a507f0641888.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e65a0bfc090a3e3bb76713a4512e97",
+    "url": "/sbc-rating-combinations/index.html"
+  },
+  {
+    "revision": "38c3ea6095cfaedd60cf",
+    "url": "/sbc-rating-combinations/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "81863ddff3146c0c74ca",
+    "url": "/sbc-rating-combinations/static/css/main.50a50f01.chunk.css"
+  },
+  {
+    "revision": "38c3ea6095cfaedd60cf",
+    "url": "/sbc-rating-combinations/static/js/2.574b5e2a.chunk.js"
+  },
+  {
+    "revision": "81863ddff3146c0c74ca",
+    "url": "/sbc-rating-combinations/static/js/main.f1975048.chunk.js"
+  },
+  {
+    "revision": "cfcdf83525bb292d8f47",
+    "url": "/sbc-rating-combinations/static/js/runtime-main.d4898cf7.js"
+  }
+]);
